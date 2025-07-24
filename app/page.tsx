8a70e7a0f60cbdbfa356b9b568752dec9cb498d9
azkriven <azkriven17@gmail.com>
2025-07-24 17:09:04 +0800

    added toto

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
-import React from "react";
+import { getUser } from "@/actions/user-actions";
+import Todos from "@/components/todos";
+import { currentUser } from "@clerk/nextjs/server";
+import Image from "next/image";
 
-export default function page() {
-  return <div>page</div>;
+export default async function Home() {
+  const user: any = await currentUser();
+  if (!user) return;
+  const fetchedData = await getUser(user?.id);
+  console.log(fetchedData);
+
+  return (
+    fetchedData && (
+      <main className="flex  items-center justify-between ">
+        <Todos todos={fetchedData[0].todos} user={fetchedData[0]} />
+      </main>
+    )
+  );
 }
